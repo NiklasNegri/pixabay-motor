42199e5f1804c7a49aab8bd346cf4f0644610b3f
NiklasNegri <69984406+NiklasNegri@users.noreply.github.com>
2022-02-10 14:46:12 +0100

    added js file, changed html
    button added to html

---
+++ search.js
@@ -0,0 +1,17 @@
+/* api info
+
+search images:
+https://pixabay.com/api/?key=25628261-88fe3cd1e6d3db0e5352b21b2&q= +
+whatever + isTypedInTheForm +
+&image_type=photo
+
+if its a photo
+
+*/
+
+// function to get form value
+
+function getUserFormInput() {
+    let formInput = document.getElementById("searchForm").TEXT_NODE;
+    alert('Form input =' + formInput);
+}
