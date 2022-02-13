710efc245e569cf704736c95e7b2547363f5e286
NiklasNegri <69984406+NiklasNegri@users.noreply.github.com>
2022-02-13 20:56:36 +0100

    js fixed
    can now search and first 10 images will be added

--- script.js
+++ script.js
@@ -5,10 +5,27 @@ function CreateAPIstring() {
     // needs an if statement if chosen color is empty for functionality
 }
 
+// fixa parameterar för att lägga in taggar, img och fotograf info
+function addPictures(imgUrl, tags, photographer) {
+    let image = document.createElement('img');
+    image.setAttribute("src", imgUrl);
+    let imageTags = document.createElement('label');
+    imageTags.textContext = tags;
+    let imagePhotographer = document.createElement('h3');
+    imagePhotographer.textContext = photographer;
+    
+    let li = document.createElement('li');
+    li.appendChild(image, tags, photographer);
+    results.append(li);
+}
+
 async function FetchJson() {
     const response = await fetch(CreateAPIstring());
     const data = await response.json()
-    return data;
+    for (var i = 0; i < 10; i++) {
+        console.log(data.hits[i]);
+        addPictures(data.hits[i].previewURL, data.hits[i].tags, data.hits[i].user);
+    }
 }
 
 // skapa en metod som tar en array med json objekt och isolerar bilderna
@@ -22,7 +39,6 @@ let results = document.querySelector('#results')
 searchButton.onclick = event => {
     // get the json data from api by sending 
     FetchJson();
-    
     // funktion för att visa alla bilder i listorna i html/dom
 
     // test: hitta värden på html elementen
