439b78135400e1921fa3a76f95a098ee7f8b1d1b
NiklasNegri <69984406+NiklasNegri@users.noreply.github.com>
2022-02-13 17:52:06 +0100

    changes to html and js

--- script.js
+++ script.js
@@ -1,31 +1,28 @@
 function CreateAPIstring() {
     return 'https://pixabay.com/api/?key=25628261-88fe3cd1e6d3db0e5352b21b2&q=' + 
     searchForm.value + '+' + chosenColor.value + '&image_type=photo';
+
+    // needs an if statement if chosen color is empty for functionality
 }
 
 async function FetchJson() {
     const response = await fetch(CreateAPIstring());
-    const data = await response.json();
-    console.log(data.hits);
+    const data = await response.json()
     return data;
 }
 
-async function IsolatePicture() {
-    const fetchedData = await FetchJson();
-
-    for (let index = 0; index < fetchedData.length; index++) {
-        const imageUrl = fetchedData[index].userImageURL;
-    }
-}
 // skapa en metod som tar en array med json objekt och isolerar bilderna
 // sen stoppar in dom i mitt li element på sidan
 
 let searchButton = document.querySelector('button');
 let searchForm = document.querySelector('input');
 let chosenColor = document.querySelector('select');
+let results = document.querySelector('#results')
 
 searchButton.onclick = event => {
-    IsolatePicture();
+    // get the json data from api by sending 
+    FetchJson();
+    
     // funktion för att visa alla bilder i listorna i html/dom
 
     // test: hitta värden på html elementen
